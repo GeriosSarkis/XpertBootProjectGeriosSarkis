4fee3c8853d5ed34a5c663ea9fbff2bd760e1614
bob <bob.xpertbot@gmail.com>
2024-08-27 08:52:27 +0000

    naming fixes

--- database/factories/MediaFactory.php
+++ database/factories/MediaFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Media>
  */
-class mediaFactory extends Factory
+class MediaFactory extends Factory
 {
     /**
      * Define the model's default state.
