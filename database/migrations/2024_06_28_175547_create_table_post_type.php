67f305828f51a22eec0802403f589e612efd26c1
Your Name <geriossarkis3@gmail.com>
2024-10-06 19:11:07 +0300

    udpte the use in posty type igrations

--- database/migrations/2024_06_28_175547_create_table_post_type.php
+++ database/migrations/2024_06_28_175547_create_table_post_type.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Models\Category;
+use App\Models\CustomRole;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\_PostType;
+
 
 return new class extends Migration
 {
@@ -15,7 +16,7 @@ return new class extends Migration
     {
         Schema::create('post_types', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\CustomRole::class)->default(0);
+            $table->foreignIdFor(CustomRole::class)->default(0);
 
 
 
